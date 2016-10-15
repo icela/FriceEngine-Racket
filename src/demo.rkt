0dd4423e928d0d9b78d4839c5cb070de8e883ec9
ice1000 <ice1000kotlin@foxmail.com>
2016-10-15 22:47:27 +0800

    Add gitignore

--- src/demo.rkt
+++ src/demo.rkt
@@ -4,7 +4,8 @@
                 (bounds 200 200 600 500)
                 (rectangle #:x 100 #:y 50 #:width 300 #:height 200 #:id "test")
                 (oval #:x 100 #:y 100)
-                (text #:content "hello world!!!")
+               ; (text #:content "hello world!!!")
+                (text #:content "Fuck code forces!")
                 (when-clicking-thunk #:object "test"
                                      #:thunk (lambda ()
                                        (tell "test" 'set 'x (+ (tell "test" 'get 'x)
